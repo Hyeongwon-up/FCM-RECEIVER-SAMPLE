7caead11820a748279443b70ff3e8545c4c47452
Hyeongwon-up <leehwdatabase@gmail.com>
2022-08-20 13:21:00 +0900

    feat: init commit

---
+++ FCM-RECEIVER-SAMPLE/src/main/java/com/example/fcmreceiversample/FcmReceiverSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.fcmreceiversample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcmReceiverSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcmReceiverSampleApplication.class, args);
+    }
+
+}
---
+++ FCM-RECEIVER-SAMPLE/src/test/java/com/example/fcmreceiversample/FcmReceiverSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fcmreceiversample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcmReceiverSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
